dc1ae9f98e220d484b25cf93926792e75e1e7089
mia <cocoball200@gmail.com>
2020-12-31 00:07:09 +0900

    set up Route

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
 
 export default () => (
     <Router>
-        <>
+        <Switch>
             <Route path="/" exact component={Home}></Route>
             <Route path="/tv" exact component={TV}></Route>
             <Route path="/search" exact component={Search}></Route>
-        </>
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 )
