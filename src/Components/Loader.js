6e4c7f9a08e5732dd6d08271c03a152efa3e6336
mia <cocoball200@gmail.com>
2021-01-07 23:05:31 +0900

    style and presenters

---
+++ src/Components/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+export default () => (
+    <Container>
+        <span role="img" aria-label="Loading">⏰</span>
+    </Container>
+);
